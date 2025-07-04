0d3dd5d0d016013171b2435642c794978f5cd378
Ashish Sharmae <sharmaashishlfps@gmail.com>
2025-07-05 00:05:45 +0400

    deployed database

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ app.use(cors({
     'http://localhost:3000',
     'http://localhost:3001', 
     'http://localhost:5173',
-    'http://localhost:5174'
+    'http://localhost:5174',
+    'http://localhost:35676',
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
